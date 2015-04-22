cbeec4cbd1c2e2bdb9f9f24459ea4ea1db6516c9
Isaac Dudek <isaac@isaacdudek.com>
2015-04-21 17:11:49 -0700

    Add authentication spec helpers

---
+++ spec/support/authentication.rb
@@ -0,0 +1,20 @@
+shared_context 'authentication', authentication: true do
+  let(:sign_in?) {true}
+
+  before {sign_in current_user if sign_in?}
+
+  context 'unauthenticated' do
+    let(:sign_in?) {false}
+
+    describe 'response' do
+      subject {response}
+
+      it {should have_http_status(:redirect)}
+      it {should redirect_to(new_user_session_path)}
+    end
+
+    describe 'flash' do
+      it {should set_flash[:alert].to('You need to sign in or sign up before continuing.')}
+    end
+  end
+end
